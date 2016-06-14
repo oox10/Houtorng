69f65b01537e978a100efe1517c790745a7a4f13
unarus <oos0.0a@gmail.com>
2016-06-14 19:21:13 +0800

    fix language change

--- mvc/templates/includes/menu.php
+++ mvc/templates/includes/menu.php
@@ -13,6 +13,11 @@ if(count($classlv)){
   }	
 }
 
+
+echo "<div style='display:none;'>";
+var_dump($product_level);
+echo "</div>";
+
 $menu_def = '<?xml version="1.0" encoding="UTF-8"?>
 <menu>
   <menu_item href="index.php" label="${Home}">
@@ -26,7 +31,6 @@ $menu_def = '<?xml version="1.0" encoding="UTF-8"?>
   <menu_item href="index.php?act=contact" label="${Contact}"></menu_item>
   <menu_item href="index.php?act=sitemap" label="${SiteMap}"></menu_item>
 </menu>';
-
 
 $menu = simplexml_load_string($menu_def);
 
